5387ac83871807e41d2fce5feaad8a3bd39a38a0
karankidarsharma <mearthkid@gmail.com>
2020-05-05 07:14:11 +1200

    Mailer

--- mail.php
+++ mail.php
@@ -21,8 +21,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'mearthkid@gmail.com';                     // SMTP username
-    $mail->Password   = 'mylifeover1';                               // SMTP password
+    $mail->Username   = 'Enter Email here';                     // SMTP username
+    $mail->Password   = 'Enter password here';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
